efa8b17e57344a18916c65418953b402a41c1a7f
Error 4 You <102403979+Error-4-You@users.noreply.github.com>
2022-04-02 07:45:47 +0530

    Create Constants.java

---
+++ src/Constants.java
@@ -0,0 +1,28 @@
+/**
+ * Error 4 You
+ *
+ * @ClassName: Constants
+ * @Description: E4Y
+ * @author: Bruce Young
+ * @date: 2022.04.
+ */
+public class Constants {
+
+    public static int ORIGINAL_COUNT = 50;
+    public static float BROAD_RATE = 0.8f;
+    public static float SHADOW_TIME = 140;
+    public static int HOSPITAL_RECEIVE_TIME = 10;
+    public static int BED_COUNT = 1000;
+    
+
+    public static float u = 0.99f;
+    public static int CITY_PERSON_SIZE = 5000;
+    public static float FATALITY_RATE = 0.50f;
+    public static int DIE_TIME = 100;
+    public static double DIE_VARIANCE = 1;
+ 
+
+    public static final int CITY_WIDTH = 700;
+    public static final int CITY_HEIGHT = 800;
+
+}
